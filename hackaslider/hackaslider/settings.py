1e7ab01d4410db03172df92cd70b1d7911f99985
Michael Anderson <michaelanderson@exosite.com>
2015-11-07 07:38:20 -0600

    Add crispy forms.

--- hackaslider/hackaslider/settings.py
+++ hackaslider/hackaslider/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hackaslider',
+    'crispy_forms',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -102,3 +103,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
